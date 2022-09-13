75eeb81bc11fccd17129047fd1c451099d66ab05
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-09-13 17:04:33 -0500

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module to consume API"""
+"""Write a Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her TODO
+list progress."""
 
 
 if __name__ == "__main__":
